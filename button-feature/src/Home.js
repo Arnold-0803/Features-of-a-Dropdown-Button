0b30bca0ad6f16e8d0328d5360b9b4d8127dd159
Arnold_Kells <arnoldkells4@gmail.com>
2024-02-22 16:08:31 +0300

    added the button & the list features

--- button-feature/src/Home.js
+++ button-feature/src/Home.js
@@ -1,7 +1,25 @@
+import {IoIosArrowDropdownCircle, IoIosArrowDropupCircle} from "react-icons/io";
+
 const Home = () => {
   return (
     <div className="home">
       <h2>Drop Down List</h2>
+      <button 
+        style={{
+          padding: "15px 20px",
+          fontSize: "20px"
+        }}
+      >Click
+        <IoIosArrowDropupCircle id="up"/>
+        <IoIosArrowDropdownCircle id="down"/>
+      </button>
+      <ul>
+        <li>List a</li>
+        <li>List b</li>
+        <li>List c</li>
+        <li>List d</li>
+        <li>List e</li>
+      </ul>
     </div>
   );
 }
