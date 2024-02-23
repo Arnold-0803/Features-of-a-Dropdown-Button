68623b45e466af11fd97f64b3572e570fbc54da8
Arnold_Kells <arnoldkells4@gmail.com>
2024-02-23 03:14:30 +0300

    added button & list functions

--- button-feature/src/Home.js
+++ button-feature/src/Home.js
@@ -1,19 +1,23 @@
+import { useState } from "react";
 import {IoIosArrowDropdownCircle, IoIosArrowDropupCircle} from "react-icons/io";
 
 const Home = () => {
+
+  const [display, setDisplay] = useState(true);
+
+  const showDisplay = () => {
+    setDisplay((prev) => !prev);
+  }
+
   return (
     <div className="home">
       <h2>Drop Down List</h2>
-      <button 
-        style={{
-          padding: "15px 20px",
-          fontSize: "20px"
-        }}
-      >Click
+      <button onClick={() => {showDisplay()}}>
+        Click
         <IoIosArrowDropupCircle id="up"/>
         <IoIosArrowDropdownCircle id="down"/>
       </button>
-      <ul>
+      <ul className={display ? "active" : ""}>
         <li>List a</li>
         <li>List b</li>
         <li>List c</li>
