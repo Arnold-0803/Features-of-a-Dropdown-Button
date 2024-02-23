6cfd300b4b11588436aa59c6f67a1244fca0ceb3
Arnold_Kells <arnoldkells4@gmail.com>
2024-02-23 12:15:19 +0300

    addded dynamic response function on button icons

--- button-feature/src/Home.js
+++ button-feature/src/Home.js
@@ -4,18 +4,25 @@ import {IoIosArrowDropdownCircle, IoIosArrowDropupCircle} from "react-icons/io";
 const Home = () => {
 
   const [display, setDisplay] = useState(true);
+  const [up, setUp] = useState(false);
+  const [down, setDown] = useState(true);
 
   const showDisplay = () => {
     setDisplay((prev) => !prev);
+    setUp((prev) => !prev);
+    setDown((prev) => !prev);
+
   }
+
+  
 
   return (
     <div className="home">
       <h2>Drop Down List</h2>
       <button onClick={() => {showDisplay()}}>
         Click
-        <IoIosArrowDropupCircle id="up"/>
-        <IoIosArrowDropdownCircle id="down"/>
+        <IoIosArrowDropupCircle id="up-icon" className={up ? "active" : ""}/>
+        <IoIosArrowDropdownCircle id="down-icon" className={down ? "inactive" : ""}/>
       </button>
       <ul className={display ? "active" : ""}>
         <li>List a</li>
